284a5ccd6c74eb334eea9cebb85599340ade6119
Ramadan <daudalaramadan2@gmail.com>
2024-06-11 23:20:21 +0000

    tech stack

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -8,37 +8,37 @@ const Experience = () => {
       <div className="experience-wrap">
         <div className="ex-text flex flex-col justify-center items-center">
           <div className="text">Explore My</div>
-          <div className="sub-text text-3xl">Experience</div>
+          <div className="sub-text text-3xl tracking-widest">Experience</div>
         </div>
       </div>
       <div className="container-wrapper">
-        <div className="frontend">
-          <div className="front">
-            FrontEnd Development  
+        <div className="frontend flex flex-col justify-center items-center">
+          <div className="front ">
+            Tech Stack 
           </div>
           <div className="1">
             <PiSealCheckFill />
-            <h3>html</h3>
+            <h3>REACT JS</h3>
             <p>Experienced</p>
           </div>
           <div className="2">
             <PiSealCheckFill />
-            <h3>html</h3>
-            <p>Experienced</p>
+            <h3>NODE JS</h3>
+            <p>Intermediate</p>
           </div>
           <div className="3">
             <PiSealCheckFill />
-            <h3>html</h3>
+            <h3>MONGO DB</h3>
             <p>Experienced</p>
           </div>
           <div className="4">
             <PiSealCheckFill />
-            <h3>html</h3>
-            <p>Experienced</p>
+            <h3>EXPRESS</h3>
+            <p>Beginner</p>
           </div>
           <div className="5">
             <PiSealCheckFill />
-            <h3>html</h3>
+            <h3>Tailwind CSS</h3>
             <p>Experienced</p>
           </div>
           <div className="6">
@@ -46,21 +46,7 @@ const Experience = () => {
             <h3>html</h3>
             <p>Experienced</p>
           </div>
-          <div className="7">
-            <PiSealCheckFill />
-            <h3>html</h3>
-            <p>Experienced</p>
-          </div>
-          <div className="8">
-            <PiSealCheckFill />
-            <h3>html</h3>
-            <p>Experienced</p>
-          </div>
-          <div className="9">
-            <PiSealCheckFill />
-            <h3>html</h3>
-            <p>Experienced</p>
-          </div>
+          
         </div>
         <div className="backend"></div>
       </div>
