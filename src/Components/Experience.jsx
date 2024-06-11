f7914f973521e55357ff16489d2327f1bd1ebd18
Ramadan <daudalaramadan2@gmail.com>
2024-06-11 22:26:05 +0000

    experience

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import './Experience.css'
+import { PiSealCheckFill } from "react-icons/pi";
 
 const Experience = () => {
   return (
-    <div className='bg-red-500'>Experience</div>
+    <div className='experience-wrapper  bg-red-500'>
+      <div className="experience-wrap">
+        <div className="ex-text flex flex-col justify-center items-center">
+          <div className="text">Explore My</div>
+          <div className="sub-text text-3xl">Experience</div>
+        </div>
+      </div>
+      <div className="container-wrapper">
+        <div className="frontend">
+          <div className="front">
+            FrontEnd Development  
+          </div>
+          <div className="1">
+            <PiSealCheckFill />
+            <h3>html</h3>
+            <p>Experienced</p>
+          </div>
+          <div className="2">
+            <PiSealCheckFill />
+            <h3>html</h3>
+            <p>Experienced</p>
+          </div>
+          <div className="3">
+            <PiSealCheckFill />
+            <h3>html</h3>
+            <p>Experienced</p>
+          </div>
+          <div className="4">
+            <PiSealCheckFill />
+            <h3>html</h3>
+            <p>Experienced</p>
+          </div>
+          <div className="5">
+            <PiSealCheckFill />
+            <h3>html</h3>
+            <p>Experienced</p>
+          </div>
+          <div className="6">
+            <PiSealCheckFill />
+            <h3>html</h3>
+            <p>Experienced</p>
+          </div>
+          <div className="7">
+            <PiSealCheckFill />
+            <h3>html</h3>
+            <p>Experienced</p>
+          </div>
+          <div className="8">
+            <PiSealCheckFill />
+            <h3>html</h3>
+            <p>Experienced</p>
+          </div>
+          <div className="9">
+            <PiSealCheckFill />
+            <h3>html</h3>
+            <p>Experienced</p>
+          </div>
+        </div>
+        <div className="backend"></div>
+      </div>
+    </div>
   )
 }
 
