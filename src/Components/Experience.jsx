1af73dcedbb160273638fcee32a10cfbc6904757
Ramadan <daudalaramadan2@gmail.com>
2024-06-12 00:01:35 +0000

    done with experience section

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -4,49 +4,59 @@ import { PiSealCheckFill } from "react-icons/pi";
 
 const Experience = () => {
   return (
-    <div className='experience-wrapper  bg-red-500'>
+    <div className='experience-wrapper  '>
       <div className="experience-wrap">
         <div className="ex-text flex flex-col justify-center items-center">
           <div className="text">Explore My</div>
           <div className="sub-text text-3xl tracking-widest">Experience</div>
-        </div>
-      </div>
-      <div className="container-wrapper">
-        <div className="frontend flex flex-col justify-center items-center">
           <div className="front ">
             Tech Stack 
           </div>
-          <div className="1">
-            <PiSealCheckFill />
+        </div>
+      </div>
+      <div className="container-wrapper  ">
+        
+        <div className="frontend grid grid-cols-2 gap-4 justify-center items-center">
+          <div className="1 flex flex-col justify-center items-center mt-4">
+            <PiSealCheckFill size={30}/>
             <h3>REACT JS</h3>
             <p>Experienced</p>
           </div>
-          <div className="2">
-            <PiSealCheckFill />
+          <div className="2 flex flex-col justify-center items-center mt-4">
+            <PiSealCheckFill size={30}/>
             <h3>NODE JS</h3>
             <p>Intermediate</p>
           </div>
-          <div className="3">
-            <PiSealCheckFill />
+          <div className="3 flex flex-col justify-center items-center">
+            <PiSealCheckFill size={30}/>
             <h3>MONGO DB</h3>
             <p>Experienced</p>
           </div>
-          <div className="4">
-            <PiSealCheckFill />
+          <div className="4 flex flex-col justify-center items-center">
+            <PiSealCheckFill size={30}/>
             <h3>EXPRESS</h3>
             <p>Beginner</p>
           </div>
-          <div className="5">
-            <PiSealCheckFill />
+          <div className="5 flex flex-col justify-center items-center">
+            <PiSealCheckFill size={30}/>
             <h3>Tailwind CSS</h3>
             <p>Experienced</p>
           </div>
-          <div className="6">
-            <PiSealCheckFill />
-            <h3>html</h3>
+          <div className="6 flex flex-col justify-center items-center">
+            <PiSealCheckFill size={30}/>
+            <h3>Git</h3>
             <p>Experienced</p>
           </div>
-          
+          <div className="6 flex flex-col justify-center items-center mb-4">
+            <PiSealCheckFill size={30}/>
+            <h3>GitLab</h3>
+            <p>Experienced</p>
+          </div>
+          <div className="6 flex flex-col justify-center items-center mb-4">
+            <PiSealCheckFill size={30}/>
+            <h3>VS Code</h3>
+            <p>Experienced</p>
+          </div>       
         </div>
         <div className="backend"></div>
       </div>
